373aca22717420b63637ca743a9bcc4719fd4f7d
clutchydsb <33865894+clutchydsb@users.noreply.github.com>
2019-04-12 19:08:36 +0900

    Update Migrations.sol
    Update

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
-  // dsdsdd
+  
   constructor() public {
     owner = msg.sender;
   }
